2842281d6700d8c5430164132ee804235fa3edd2
Code Writter <codewritter.1+gitwork@gmail.com>
2024-12-02 01:02:25 +0530

    Added About blog project

--- Portfolio/routes.py
+++ Portfolio/routes.py
@@ -5,3 +5,15 @@ from flask import render_template
 @app.route('/home')
 def home_page():
     return render_template('index.html')
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects_page():
+    return render_template('projects.html')
+
+@app.route('/blogs')
+def blogs_page():
+    return render_template('blogs.html')
